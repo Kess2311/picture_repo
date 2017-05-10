2d783c07796b3c051d907a3d528acd1f027ae0d3
Kess2311 <lxk6020@rit.edu>
2017-05-10 19:33:09 -0400

    Website inital setup complete
    All work has been merged from old repo to this one

--- my_site/index/views.py
+++ my_site/index/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
 
-# Create your views here.
+def home(request):
+    return render(request, 'index/index.html')
+
+def about_page(request):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'media/lucas_resume.pdf')
+    return render(request, 'index/about.html', {'file':file_path})
+
+
+def contact_me(request):
+    return render(request, 'index/contact.html')
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -1,4 +1,4 @@
-"""my_site URL Configuration
+"""personal_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^pictures/', include('pictures.urls')),
+    url(r'^index/', include('index.urls')),
 ]
