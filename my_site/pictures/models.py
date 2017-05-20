ef5b0513c71adf059669012553b02c20993cfde9
Kess2311 <lxk6020@rit.edu>
2017-05-19 22:29:55 -0400

    Got a basic working copy of the pictures
    form and viewing of picture database is set up. Must get the actual working form submmission and display of 20 photos per page, etc.

--- my_site/pictures/models.py
+++ my_site/pictures/models.py
@@ -1,4 +1,35 @@
 from django.db import models
+from .validator import validate_file_extension
+import datetime, os
+
 
 class Picture(models.Model):
-    pass
+    CHITTENANGO = 'C'
+    NY = 'N'
+    CARS = 'H'
+    COLORADO = 'D'
+    NIGHT = 'B'
+
+    PICTURE_CHOICES = (
+        (CARS, 'Cars'),
+        (CHITTENANGO, 'Chittenango'),
+        (COLORADO, 'Colorado'),
+        (NIGHT, 'Night'),
+        (NY, 'New York'),
+    )
+
+    def content_file_name(self, filename):
+        """
+        Used to rename the file to pk of the doctor and test
+        gives a unique name for each file
+        :param filename: original filename
+        :return: new filename
+        """
+        ext = filename.split('.')[-1]
+        filename = "%s_%s.%s" % (filename, self.id, ext)
+        return os.path.join('pictures/static/pictures/', filename)
+
+    description = models.TextField(default='Uploaded Photo')
+    location = models.CharField(max_length=1, choices=PICTURE_CHOICES, default='A')
+    date = models.DateField(default=datetime.date.today)
+    file = models.FileField(default=None, upload_to=content_file_name, validators=[validate_file_extension])
